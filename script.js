30c27e26cd4c45e9a4d3732e49bd3991981ea6d8
ArmandoMarino <scrivania.lavoro@gmail.com>
2023-02-09 16:46:11 +0100

    test render call API

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+console.log('vue ok', Vue);
 const { createApp } = Vue;
 const apiUri = 'http://localhost/php-dischi-json/discs.php';
 const app = createApp({
     data() {
         return {
-
+            movies: []
         }
+    },
+    created() {
+        axios.get(apiUri).then(res => {
+            this.movies = res.data;
+        })
     }
 })
 
